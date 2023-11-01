94e9d544b189a818c7d485732c47ee5b7548aa61
Ukbenoz <149002105+Ukbenoz@users.noreply.github.com>
2023-10-31 21:39:51 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#this is my first GitHub addition
 def encoder(password):
     encoded_string = ''
 
@@ -13,7 +14,7 @@ def encoder(password):
         if password[i] == '5':
             encoded_string += '8'
         if password[i] == '6':
-            encoded_string == '9'
+            encoded_string += '9'
         if password[i] == '7':
             encoded_string += '0'
         if password[i] == '8':
@@ -23,21 +24,36 @@ def encoder(password):
 
     return encoded_string
 
-
-
-
 def main():
-    print('Menu')
-    print('-' * 13)
-    print('1. Encode')
-    print('2. Decode')
-    print('3. Quit')
-    user_select = int(input('Please enter an option:'))
-    password_select = int(input('Please enter password to encode:'))
-    if user_select == 1:
-        y = encoder(password_select)
-        print(y)
-    print('Your password has been stored and encoded!')
+    while True:
+        print('Menu')
+        print('-' * 13)
+        print('1. Encode')
+        print('2. Decode')
+        print('3. Quit')
+        user_select = int(input('Please enter an option:'))
+        password_select = input('Please enter password to encode:')
 
+        if user_select == 1:
+            y = encoder(password_select)
+            print(y)
+            print('Your password has been stored and encoded!')
+        elif user_select == 2:
+            decoded_password = decode_password(password_select)
+            print(decoded_password)
+        elif user_select == 3:
+            break
 
-#encoded_string += str(int(password[i]) + 3)
+def decode_password(password):
+    decode_password = ""
+    for digit in password:
+        if digit.isdigit():
+            new_digit = str((int(digit) - 3) % 10)
+            decode_password += new_digit
+        else:
+            decode_password += digit
+
+    return decode_password
+
+if __name__ == '__main__':
+    main()
